f446badf8dfab30811960a2847f4c97bcd4e0357
Emirhan Yaman <yaman.emrhn@gmail.com>
2024-10-13 03:03:31 +0300

    initialize react vite/shadcn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button variant="default" size="sm">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
